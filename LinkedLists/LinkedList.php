94f06751bb576a368f2b23012749fda508ff5d2a
Wellington Braga <wellington.braga.inatel@gmail.com>
2018-08-18 01:29:55 -0300

    Linked List 05: Creation of the method to remove Node from Linked List The function must receives a parameter data which is the value which the node that should be deleted stores. Then we iterate over the list looking for this value and when we found it, we remove it.

--- LinkedLists/LinkedList.php
+++ LinkedLists/LinkedList.php
@@ -118,4 +118,37 @@ class LinkedList
         $node->next = $newNode;
     }
 
+    /**
+     * Remove the node which contains the first occurence of data
+     *
+     * @param int $data
+     *
+     * @return bool If the node was found and removed
+     */
+    public function deleteNode(int $data) {
+
+        //First we check if head is the Node that should be deleted
+        if ($this->head->data === $data) {
+            $this->head = $this->head->next;
+            return true;
+        }
+
+        // If it is not, we iterate over the list until find the previous Node of the
+        // the one we are going to delete
+        $node = $this->head;
+
+        while($node->next !== null && $node->next->data !== $data) {
+            $node = $node->next;
+        }
+
+        // Check if the next element of the node is null, if so, then we
+        // achieve the end of the list without find the element that should be removed
+        if ($node->next === null) {
+            return false;
+        }
+
+        $node->next = $node->next->next;
+        return true;
+
+    }
 }
